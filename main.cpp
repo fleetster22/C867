f5a90c0a20fe40c7d5660d224caa941466d3b315
Critters <fleetster22@gmail.com>
2022-01-23 21:40:21 -0500

    fixed spacing issue

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
     "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
     "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-    "A5,Anaka,Norfleet,anorfl2@wgu.edu,48,34,30,32,SOFTWARE",
+    "A5,Anaka,Norfleet,anakanorfleet@gmail.com,48,34,30,32,SOFTWARE",
     };
 
     
